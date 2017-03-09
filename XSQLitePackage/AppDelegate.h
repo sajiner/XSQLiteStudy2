45fa2fa0d4e4c6a6ffe79b49bdc48fa8220c9594
sajiner <739134362@qq.com>
2017-03-09 14:53:07 +0800

    初始化

---
+++ XSQLitePackage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XSQLitePackage
+//
+//  Created by sajiner on 2017/3/9.
+//  Copyright © 2017年 sajiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XSQLitePackage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XSQLitePackage
+//
+//  Created by sajiner on 2017/3/9.
+//  Copyright © 2017年 sajiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
